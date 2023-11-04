2ed9ff169452b6a32d8892e6523f917cc813bb67
angelm1974 <mariusz980@gmail.com>
2023-11-04 09:38:56 +0100

    Stworzenie metody kiełbasa

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+internal class Program
+{
+    /// <summary>
+    /// Procedura główna aplikacji
+    /// </summary>
+    /// <param name="args">Parametry wejsciowe w postaci tablicy tekstowej </param>
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Kielbasa();
+        Console.ReadLine();
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private static void Kielbasa()
+    {
+        Console.WriteLine($"Kielbasa");
+/*
+        TextReader tr = File.OpenText("D:\\Programy\\aplikacje_cs\\aplikacjakonsolowa1\\README.md");
+        Console.WriteLine(tr.ReadToEnd());
+*/
+        Console.ReadLine();
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # aplikacjakonsolowa1
+To nie by³a reklama tenisa na najwy¿szym poziomie. 
+Iga Œwi¹tek musia³a bowiem walczyæ nie tylko z Ons Jabeur, 
+ale równie¿ z silnymi podmuchami wiatru szalej¹cego na korcie w Cancun. 
+Polka ostatecznie wygra³a 6:1, 6:2 i tym samym wygra³a swoj¹ grupê bez straty seta. 
+A to oznacza, ¿e w sobotnim pó³finale czeka nas prawdziwy hit. 
+Na drodze Œwiatek stanie liderka rankingu WTA Aryna Sabalenka. 22-latka powalczy o to, 
+by mieæ szansê na powrót na szczyt klasyfikacji! Spotkanie odbêdzie siê 
+w nocy z soboty na niedzielê nie przed pó³noc¹.
