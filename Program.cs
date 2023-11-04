19bc9223c3d08bb32c4ce971ed8a66063dde8442
angelm1974 <mariusz980@gmail.com>
2023-11-04 11:09:00 +0100

    druga czesc

--- Program.cs
+++ Program.cs
@@ -1,29 +1,38 @@
 ﻿using System.IO;
 internal class Program
 {
+
+    static int a=0, b=0;
+    static string pobrana;
+
     /// <summary>
     /// Procedura główna aplikacji
     /// </summary>
     /// <param name="args">Parametry wejsciowe w postaci tablicy tekstowej </param>
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Kielbasa();
+        try
+        {
+            Console.WriteLine("Podaj liczbę a: ");
+            a = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("Podaj liczbę b: ");
+            b = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine(a+b);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Stary podałeś coś nie tak.");
+
+        }
+
+
+
         Console.ReadLine();
 
     }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    private static void Kielbasa()
-    {
-        Console.WriteLine($"Kielbasa");
-/*
-        TextReader tr = File.OpenText("D:\\Programy\\aplikacje_cs\\aplikacjakonsolowa1\\README.md");
-        Console.WriteLine(tr.ReadToEnd());
-*/
-        Console.ReadLine();
-    }
+    
+    
 
 }
